aeef3f7c12ef462d421fa9f4c15bb1454a225e61
Sharpiro <dsharpbb09@gmail.com>
2017-09-22 12:27:56 -0400

    Feature/nuget fixes (#3)
    * fix where calls would fail if token had no "issue date"
    
    * updated to netstandard2.0, removed logging dependency
    
    * added system.net.http nuget package to net46 only
    updated version

--- FacebookApi.Core/Services/AdminApiService.cs
+++ FacebookApi.Core/Services/AdminApiService.cs
@@ -1,6 +1,7 @@
 ﻿using FacebookApi.Core.Models;
 using FacebookApi.Core.Tools;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -12,13 +13,18 @@ namespace FacebookApi.Core.Services
 
         public async Task<TokenInfoModel> GetTokenInfo(string userToken)
         {
+            if (string.IsNullOrEmpty(userToken)) throw new ArgumentNullException(nameof(userToken));
+
             var path = $"debug_token";
             var queryParameters = new Dictionary<string, string> { ["input_token"] = userToken };
             var data = await RequestService.GetAsync(path, queryParameters, appendToken: true);
             var jObject = JObject.Parse(data)["data"];
+
+            var issuedAt = (int?)jObject["issued_at"];
             var tokenInfo = new TokenInfoModel
             {
-                RequestedUtc = DotnetExtensions.FromEpochTime((int)jObject["issued_at"]),
+                TokenPortion = $"{userToken.Substring(0, 10)}...",
+                RequestedUtc = issuedAt.HasValue ? DotnetExtensions.FromEpochTime(issuedAt.Value) : default(DateTime?),
                 ExpiresUtc = DotnetExtensions.FromEpochTime((int)jObject["expires_at"]),
             };
             return tokenInfo;
